65989f391b7f2802b870c755c142969734fea99e
Andrei Volt <andrei@avolt.net>
2024-02-04 17:42:03 +0100

    package for Nix

--- default.nix
+++ default.nix
@@ -1,18 +1,17 @@
-with import <nixpkgs> {};
+{ pkgs ? import <nixpkgs> {} }:
 
 pkgs.stdenv.mkDerivation {
   name = "nix-beautify";
+
   src = ./.;
 
-  phases = [ "install" ];
-
-  install = ''
+  installPhase = ''
     mkdir -p $out/bin
     mkdir -p $out/js
     cp $src/nix-beautify.js $out/js
     cat > $out/bin/nix-beautify << EOF
-    #!/usr/bin/env bash 
-    ${nodejs}/bin/node $out/js/nix-beautify.js
+    #!/usr/bin/env bash
+    ${pkgs.nodejs}/bin/node $out/js/nix-beautify.js
     EOF
     chmod u+x $out/bin/nix-beautify
   '';
